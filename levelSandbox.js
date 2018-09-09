648b075d7bd1f79d26d2253fc344bba8e984bbb9
mostafa69d <mostafa69d@gmail.com>
2018-09-09 17:50:52 -0400

    clean the code

--- levelSandbox.js
+++ levelSandbox.js
@@ -1,7 +1,3 @@
-/* ===== Persist data with LevelDB ===================================
-|  Learn more: level: https://github.com/Level/level     |
-|  =============================================================*/
-
 const level = require('level');
 const chainDB = './chaindata';
 const db = level(chainDB);
@@ -82,24 +78,6 @@ function getNumberOfRecordsInDB() {
     });
 
 }
-/* ===== Testing ==============================================================|
-|  - Self-invoking function to add blocks to chain                             |
-|  - Learn more:                                                               |
-|   https://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/  |
-|                                                                              |
-|  * 100 Milliseconds loop = 36,000 blocks per hour                            |
-|     (13.89 hours for 500,000 blocks)                                         |
-|    Bitcoin blockchain adds 8640 blocks per day                               |
-|     ( new block every 10 minutes )                                           |
-|  ===========================================================================*/
-
-
-// (function theLoop(i) {
-//     setTimeout(function() {
-//         addDataToLevelDB('Testing data');
-//         if (--i) theLoop(i);
-//     }, 100);
-// })(10);
 
 module.exports = {
     getLevelDBData,
