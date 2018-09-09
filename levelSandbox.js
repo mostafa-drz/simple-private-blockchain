53622f891ebb6530bd377ab5f5d50ee805a11e58
mostafa69d <mostafa69d@gmail.com>
2018-09-09 11:31:46 -0400

    refactor: add promises to levelSandbox

--- levelSandbox.js
+++ levelSandbox.js
@@ -8,42 +8,78 @@ const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
 function addLevelDBData(key, value) {
+    return new Promise((resolve, reject) => {
+        db.put(key, value, function(err) {
+            if (err) return reject({
+                error: {
+                    message: 'Block ' + key + ' submission failed',
+                    main: err
+                }
+            });
+            else {
+                return resolve();
+            }
+        })
+    });
 
-    db.put(key, value, function(err) {
-        if (err) return console.log('Block ' + key + ' submission failed', err);
-    })
 }
 
 // Get data from levelDB with key
 function getLevelDBData(key) {
-    db.get(key, function(err, value) {
-        if (err) return console.log('Not found!', err);
-        console.log('Value = ' + value);
-    })
+    return new Promise((resolve, reject) => {
+        db.get(key, function(err, value) {
+            if (err) {
+                return reject({
+                    error: {
+                        message: 'Not Found!',
+                        main: err
+                    }
+                })
+            }
+            return resolve(value);
+        })
+    });
+
 }
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
-    let i = 0;
-    db.createReadStream().on('data', function(data) {
-        i++;
-    }).on('error', function(err) {
-        return console.log('Unable to read data stream!', err)
-    }).on('close', function() {
-        console.log('Block #' + i);
-        addLevelDBData(i, value);
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        db.createReadStream().on('data', function(data) {
+            i++;
+        }).on('error', function(err) {
+            return reject({
+                error: {
+                    message: 'Unable to read data stream!',
+                    main: err
+                }
+            })
+        }).on('close', function() {
+            console.log('Block #' + i);
+            return addLevelDBData(i, value);
+        });
     });
+
 }
 
 function getNumberOfRecordsInDB() {
-    let i = 0;
-    db.createReadStream().on("data", function(data) {
-        i++;
-    }).on("error", function(error) {
-        return console.log(error);
-    }).on("close", function() {
-        return i;
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        db.createReadStream().on("data", function(data) {
+            i++;
+        }).on("error", function(err) {
+            return reject({
+                error: {
+                    message: 'Could not get the number of records',
+                    main: err
+                }
+            })
+        }).on("close", function() {
+            return resolve(i);
+        });
     });
+
 }
 /* ===== Testing ==============================================================|
 |  - Self-invoking function to add blocks to chain                             |
